91b6ee17ee0c1321e7c5fac00b1d90aa3098288a
mrt39 <mertoktar39@gmail.com>
2023-03-30 18:56:05 +0300

    Add basic HTML + CSS structure

--- src/index.js
+++ src/index.js
@@ -1,1 +1,55 @@
-console.log("waddup yo")
+//array for storing the projects 
+var allProjects = [];
+
+
+
+//Constructor for project objects.
+
+//The property for storing the "tasks" in it.
+
+
+//property for seeing if that's the one user has selected 
+//var currentlyOn = false
+
+
+
+
+
+//CONSTRUCTOR FOR TASK OBJECTS
+
+//name 
+
+//description
+
+//date due
+
+//importance (low-medium-high)
+
+//whether it's checked (on checkbox) or not
+//var finished = false
+
+//edit button
+
+//delete button 
+//remove.self
+
+
+
+
+//addeventlistener for "add task" button
+//when the user clicks on it, a window pops up in the middle of the DOM, everything else on the background blurs out in gray
+//and a form appears, from which the user can write and select things
+
+
+//addeventlistener for "new project" button
+//just a textfield opens up under it, the user can press name and submit
+
+
+//the functionality of the "edit" button
+//a similar window from the "add" task opens up, with the fields already filled up with info 
+//from the existing task. 
+//how do i get the info from the task though? how do i select the task? what do i pass in here?
+
+
+
+
